0318843ac1c2a38c7af47ac67f19f8498db586f9
mabindajoash <mabindajoash@gmail.com>
2025-04-16 12:08:03 +0000

    task 2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,14 @@
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then((result) => {
+      return {status: 200, body: 'success'};
+    })
+
+    .catch((error) => {
+      return new Error();
+    })
+
+    .finally(() => {
+      console.log("Got a response from the API")
+    });
+}
